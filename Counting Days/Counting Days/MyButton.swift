0392179df7481554d58a4ffa9ac6f943f786e74c
Guest User <Guest@yesifandeMacBook-Pro.local>
2018-10-27 21:51:40 -0400

    some improvement

--- Counting Days/Counting Days/MyButton.swift
+++ Counting Days/Counting Days/MyButton.swift
@@ -9,16 +9,43 @@
 import Foundation
 import UIKit
 
+protocol EmojiTileViewDelegate: class  {
+    func shouldFlip(_ mybutton: MyButton, index: Int) -> Bool
+    func didFlip(_ mybutton: MyButton, index: Int)
+}
+
 class MyButton: UIButton {
+    
+    weak var delegate: EmojiTileViewDelegate?
     
     
     
     var index = 0
+    
+    func animateFlipBack() {
+        UIButton.transition(
+            with: self,
+            duration: 0.5,
+            options: [.transitionFlipFromRight],
+            animations: {
+        })
+    }
+    
+    func animateFlip() {
+        UIButton.transition(
+            with: self,
+            duration: 0.5,
+            options: [.transitionFlipFromLeft],
+            animations: {
+                self.delegate?.didFlip(self, index: self.index)
+        })
+    }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         titleLabel?.numberOfLines = 0
         titleLabel?.adjustsFontSizeToFitWidth = true
     }
+    
     
 }
--- Counting Days/Counting Days/ViewController.swift
+++ Counting Days/Counting Days/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, EmojiTileViewDelegate {
+
+    
     
     var model : Model?
     
@@ -16,14 +18,49 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        model?.initGame(with: 24)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0..<tiles.count {
+            if let idx = model?.tilesArray[i].index{
+                tiles[i].index = idx
+                tiles[i].delegate = self
+            }
+        }
+        
+        updateAll()
     }
+    
+    
+    func updateAll() {
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func shouldFlip(_ mybutton: MyButton, index: Int) -> Bool {
+        
+        if let up = model?.isFaceUp(index){
+            if up {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+    
+    func didFlip(_ mybutton: MyButton, index: Int) {
+        updateAll()
+    }
+    
+    
 
 }
 
