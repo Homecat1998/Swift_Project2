d30085ad33cd33ba6006d3294f5812347fe205db
Zhong <zzhong9@itc-imac-27.ur.rochester.edu>
2018-10-29 14:08:44 -0400

    done

--- Counting Days/Counting Days/AppDelegate.swift
+++ Counting Days/Counting Days/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-//        loadData()
+        loadData()
         
         if let naviController = window?.rootViewController as? UINavigationController {
             if let mainViewController = naviController.viewControllers.last as? ViewController {
@@ -34,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-//        saveData()
+        saveData()
     }
 
 
